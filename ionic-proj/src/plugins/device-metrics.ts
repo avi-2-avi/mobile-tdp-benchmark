3557ea43a6fac4c468584ca5547d715b8b2cec15
Cristina <avicon95@protonmail.com>
2024-06-06 00:17:46 -0500

    Initial commit

--- ionic-proj/src/plugins/device-metrics.ts
+++
@@ -1,9 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-
-export interface DeviceMetricsPlugin {
-  getMetrics(): Promise<{ fps: number; cpuUsage: number; memoryUsage: number }>;
-}
-
-const DeviceMetrics = registerPlugin<DeviceMetricsPlugin>('DeviceMetrics');
-
-export { DeviceMetrics };
